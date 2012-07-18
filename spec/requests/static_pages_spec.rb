97478504232cc705994a63f22bce674874c4ce08
Joe Wei <secret-for-me@hotmail.com>
2012-07-18 14:21:00 +0800

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  describe "Home page" do
+    it "should have the h1 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Sample App')
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      #get static_pages_index_path
+      #response.status.should be(200)
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title',:text=>" | Home")
+    end
+
+  end
+
+  describe "Help page" do
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+   
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => ' | Help')
+    end
+
+  end
+
+  describe "About page" do
+    it "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => ' | About Us')
+    end
+  end
+
+end
